4997d3117704065e1e344671507a3285df7f7f74
Anup Rai <raileohang@gmail.com>
2024-02-02 00:06:41 +0545

    feat: setup react-router-dom; implement navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/Home';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Outlet />
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom';
+import HomePage from './pages/Home.tsx';
+import LoginPage from './pages/Login.tsx';
+import PlansPage from './pages/SubscriptionPlans.tsx';
+import BrowsePage from './pages/Browse.tsx';
+import WatchPage from './pages/Watch.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/plans" element={<PlansPage />} />
+      <Route path="/browse" element={<BrowsePage />} />
+      <Route path="/browse/watch/:id" element={<WatchPage />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
