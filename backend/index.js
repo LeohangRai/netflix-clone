64ebde0ff208034f1fedb2dc64362395a05ffb90
Anup Rai <raileohang@gmail.com>
2024-02-02 13:13:22 +0545

    feat(backend): create movies listing API

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const moviesData = require('./movies-data.json');
 
 const SERVER_PORT = process.env.PORT || 8080;
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   return res.json({
     status: 200,
     message: 'Hello there'
   });
+});
+
+app.get('/movies', (req, res) => {
+  return res.send(moviesData);
 });
 
 app.listen(SERVER_PORT, () => {
