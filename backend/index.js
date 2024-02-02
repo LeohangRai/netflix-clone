9ff3ba5396b6d475ac219373f5168082e91431a7
Anup Rai <raileohang@gmail.com>
2024-02-02 14:28:41 +0545

    feat(backend): configure and setup cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const moviesData = require('./movies-data.json');
 
 const SERVER_PORT = process.env.PORT || 8080;
+const CORS_OPTIONS = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173'
+};
+
 const app = express();
+app.use(cors(CORS_OPTIONS));
 
 app.get('/', (_req, res) => {
   return res.json({
