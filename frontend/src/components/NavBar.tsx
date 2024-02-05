8cd98a125e85b17085024dc0f21a32eac3ed197d
Anup Rai <raileohang@gmail.com>
2024-02-05 17:11:32 +0545

    fix(frontend): conditionally darken the navbar background based on page scroll point

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import NeflixLogo from '../assets/logo.png';
 
 const navButtons = [
@@ -10,9 +11,23 @@ const navButtons = [
 ];
 
 export default function NavBar() {
+  const [showBackground, setShowBackground] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 700) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    });
+  }, []);
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-16 py-6 flex items-center">
+      <div
+        className={`px-16 py-6 flex items-center ${
+          showBackground ? 'bg-black bg-opacity-80' : null
+        }`}
+      >
         <img className="h-16" src={NeflixLogo} alt="logo" />
         <div className="flex gap-7 ml-8">
           {navButtons.map((item) => (
